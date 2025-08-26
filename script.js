3b377862d99c8bf1307290ed05aa297cb18039cc
Sotee16 <josephgayflor73@gmail.com>
2025-08-26 04:22:36 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const navLink = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+// Open menu when the open button is clicked
+menuOpenButton.addEventListener("click", () =>
+    document.body.classList.add("show-mobile-menu"));
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => document.body.classList.remove("show-mobile-menu"));
+
+// Close menu when the nav Link is clicked
+navLink.forEach(link => {
+    link.addEventListener("click", () => document.body.classList.remove("show-mobile-menu"));
+})
+
+const swiper = new Swiper('.slider-wrapper', {
+
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-next',
+    },
+});
